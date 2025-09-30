cff7061a21e4524bca8d580d5921f9d2c63b0799
PatoSovietico <arthurbat120907@gmail.com>
2025-09-30 12:52:41 -0400

    cu

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScenarioModule } from './modules/scenario/scenario.module';
-import { OptionModule } from './module/option/option.module';
+import { OptionModule } from './modules/option/option.module';
 
 @Module({
   imports: [ScenarioModule, OptionModule],
